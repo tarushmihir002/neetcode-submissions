a2880de2fa98fee6bcc869251ca43aee4888767b
Tarush Mihir <mihirtarush@gmail.com>
2026-06-11 01:03:53 +0530

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m= matrix.size();
+        int n=matrix[0].size();
+        // cout<<m<<" "<<n<<endl;
+
+        int start=0,end=m*n-1;
+        while(start<=end){
+            int mid=(start+end)>>1;
+            int row=mid/n;
+            int col=mid%n;
+            if(matrix[row][col]==target) return true;
+            else if(matrix[row][col]>target) end=mid-1;
+            else start=mid+1;
+        }
+        return false;
+    }
+};
