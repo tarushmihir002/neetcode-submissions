33ef24dab08760834dd637de9071d275483656b3
Tarush Mihir <mihirtarush@gmail.com>
2026-06-19 15:59:42 +0530

    Add: concatenation-of-array - submission-0

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> ans=nums;
+        for(auto it:nums) ans.push_back(it);
+        return ans;
+    }
+};
