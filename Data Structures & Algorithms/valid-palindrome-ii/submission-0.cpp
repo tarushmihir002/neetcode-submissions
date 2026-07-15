fa7c036f609c512a08e09294ab254d692e33c5b1
Tarush Mihir <mihirtarush@gmail.com>
2026-07-15 13:50:54 +0530

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int l = 0, r = s.size() - 1;
+
+        while (l < r) {
+            if (s[l] != s[r]) {
+                return isPalindrome(s.substr(0, l) + s.substr(l + 1)) ||
+                       isPalindrome(s.substr(0, r) + s.substr(r + 1));
+            }
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+
+private:
+    bool isPalindrome(string s) {
+        int l = 0, r = s.length() - 1;
+        while (l < r) {
+            if (s[l] != s[r]) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
